34c6be483fc7c34222e2c470444824ae5c090014
Enrique Coello <enrique.coello@gmail.com>
2023-01-04 21:59:22 -0500

    Added the rank value to the card class

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -14,11 +14,9 @@ public class Card {
 
         if (rank.equals("2") || rank.equals("3") || rank.equals("4") || rank.equals("5") || rank.equals("6")) {
             value = 1;
-        }
-        else if (rank.equals("7") || rank.equals("8") || rank.equals("9")) {
+        } else if (rank.equals("7") || rank.equals("8") || rank.equals("9")) {
             value = 0;
-        }
-        else {
+        } else {
             value = -1;
         }
     }
@@ -27,9 +25,22 @@ public class Card {
         return this.rank;
     }
 
-    public String getSuti() {
+    public int getRankValue() {
+        int rankValue = 0;
+        if (rank.equals("2") || rank.equals("3") || rank.equals("4") || rank.equals("5") || rank.equals("6") || rank.equals("7") || rank.equals("8") || rank.equals("9")) {
+            rankValue = Integer.valueOf(rank);
+        } else if (rank.equals("T") || rank.equals("J") || rank.equals("Q") || rank.equals("K")) {
+            rankValue =  10;
+        } else if (rank.equals("A")) {
+            rankValue =  1;
+        }
+        return rankValue;
+    }
+
+    public String getSuit() {
         return this.suit;
     }
+
     public int getValue() {
         return value;
     }
